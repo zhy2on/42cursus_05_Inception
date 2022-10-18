ad154f1a3d292fc8144d46426e13d6cea8b9602f
zhy2on <dhwlgus5940@gmail.com>
2022-10-18 05:31:46 +0000

    Fix: nginx 환경변수 사용하도록 수정

--- srcs/requirements/nginx/tools/create_ssl.sh
+++ srcs/requirements/nginx/tools/create_ssl.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 
+sed -i "s/server_name _;/server_name $DOMAIN_NAME;/" /etc/nginx/sites-available/default
+
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 	openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
 		-keyout /etc/ssl/private/nginx.key \
 		-out /etc/ssl/certs/nginx.crt \
-		-subj "/C=KR/ST=Seoul/L=Seoul/O=wordpress/CN=jihoh.42.fr";
+		-subj "/C=KR/ST=Seoul/L=Seoul/O=wordpress/CN=$DOMAIN_NAME";
 fi
 
 exec "$@"
