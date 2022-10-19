9f3a2983e5a7920d00c189a8f9411cf4a821af98
zhy2on <dhwlgus5940@gmail.com>
2022-10-19 14:36:42 +0000

    Refactor: mariadb config file 이름 변경

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 service mysql start
 
-sed -i s/'{$WP_DB_NAME}'/$WP_DB_NAME/g /tmp/set_db.sql
-sed -i s/'{$WP_ADMIN}'/$WP_ADMIN/g /tmp/set_db.sql
-sed -i s/'{$WP_ADMIN_PASSWORD}'/$WP_ADMIN_PASSWORD/g /tmp/set_db.sql
+sed -i s/'{$WP_DB_NAME}'/$WP_DB_NAME/g /tmp/db-config.sql
+sed -i s/'{$WP_ADMIN}'/$WP_ADMIN/g /tmp/db-config.sql
+sed -i s/'{$WP_ADMIN_PASSWORD}'/$WP_ADMIN_PASSWORD/g /tmp/db-config.sql
 
-mariadb < /tmp/set_db.sql
+mariadb < /tmp/db-config.sql
 
 exec "$@"
