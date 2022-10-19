23f3c119799eaded7f51795da6b47e7766463f97
zhy2on <dhwlgus5940@gmail.com>
2022-10-19 14:41:37 +0000

    Refactor: entrypoint shell script 순서 좀 수정

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-service mysql start
-
 sed -i s/'{$WP_DB_NAME}'/$WP_DB_NAME/g /tmp/db-config.sql
 sed -i s/'{$WP_ADMIN}'/$WP_ADMIN/g /tmp/db-config.sql
 sed -i s/'{$WP_ADMIN_PASSWORD}'/$WP_ADMIN_PASSWORD/g /tmp/db-config.sql
+
+service mysql start
 
 mariadb < /tmp/db-config.sql
 
