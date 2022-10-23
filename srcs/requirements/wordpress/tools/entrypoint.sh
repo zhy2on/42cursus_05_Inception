d5de530da5d9b6b760014218621c3fa84b69a007
zhy2on <dhwlgus5940@gmail.com>
2022-10-23 19:16:56 +0000

    Fix: --skip-email option 추가 & sendmail install 빼기.

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -15,7 +15,7 @@ done
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_ADMIN --dbpass=$WP_ADMIN_PASSWORD --dbhost=mariadb --allow-root
-	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root 
 	chown -R www-data:www-data /var/www/html/wordpress
 fi
