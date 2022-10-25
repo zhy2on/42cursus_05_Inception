485cc146dab30a25299fd3a06fd052fb56029e84
zhy2on <dhwlgus5940@gmail.com>
2022-10-25 15:57:42 +0000

    Feat: add redis-cache plugin

---
+++ srcs/requirements/redis/tools/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo "*****Starting Redis Container*****"
+
+exec "$@"
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -3,20 +3,34 @@ set -e
 
 cd /var/www/html/wordpress
 
-if [ ! -f /var/www/html/wordpress/index.php ]; then
-	wp core download --allow-root
-fi
-
-# wait for mariadb
+# waiting for mariadb
 while ! mariadb -hmariadb -u$WP_ADMIN -p$WP_ADMIN_PASSWORD $WP_DB_NAME &> /dev/null; do
 	echo "Waiting for MariaDB ..."
     sleep 2
 done
 
-if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+# waiting for redis
+while ! redis-cli -h redis &> /dev/null; do
+	echo "Waiting for Redis ..."
+    sleep 2
+done
+
+if ! wp core is-installed --allow-root; then
+	wp core download --allow-root
+
 	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_ADMIN --dbpass=$WP_ADMIN_PASSWORD --dbhost=mariadb --allow-root
+	wp config set WP_REDIS_HOST redis --allow-root
+	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+	wp config set WP_CACHE true --raw --allow-root
+
 	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root 
+	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+	
+	wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+	wp redis enable --allow-root
+
 	chown -R www-data:www-data /var/www/html/wordpress
 fi
 
