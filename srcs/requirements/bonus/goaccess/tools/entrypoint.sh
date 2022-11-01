a21e680c106b53cbbefd85eb3a79f0db8c0ddeb7
zhy2on <dhwlgus5940@gmail.com>
2022-11-01 06:40:20 +0000

    Test: goaccess https 연결 테스트중

--- srcs/requirements/bonus/goaccess/tools/entrypoint.sh
+++ srcs/requirements/bonus/goaccess/tools/entrypoint.sh
@@ -7,10 +7,21 @@ while [ ! -f /var/log/nginx/wordpress_access.log ]; do
     sleep 1
 done
 
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+	openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+		-keyout /etc/ssl/private/nginx.key \
+		-out /etc/ssl/certs/nginx.crt \
+		-subj "/C=KR/ST=Seoul/L=Seoul/O=goaccess/CN=$DOMAIN_NAME";
+fi
+
 # create nginx log report page
 mkdir -p /var/www/html/wordpress/goaccess
-cd /var/log/nginx/
-goaccess -f wordpress_access.log -a > /var/www/html/wordpress/goaccess/index.html
+goaccess \
+		-f /var/log/nginx/wordpress_access.log \
+		-o /var/www/html/wordpress/goaccess/index.html \
+		--ssl-cert=/etc/ssl/certs/nginx.crt \
+		--ssl-key=/etc/ssl/private/nginx.key \
+		--real-time-html --daemonize
 chown -R www-data:www-data /var/www/html/wordpress
 
 echo "*****Starting GoAccess Container*****"
