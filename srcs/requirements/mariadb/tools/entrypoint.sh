d3b3115c066c3f37d888ba0976268ab8f5a2378b
zhy2on <dhwlgus5940@gmail.com>
2022-11-02 13:43:55 +0000

    Refactor: debian.cnf copy위치 수정

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 sed -i s/'{$WP_DB_NAME}'/$WP_DB_NAME/g /tmp/db-config.sql
 sed -i s/'{$WP_ADMIN}'/$WP_ADMIN/g /tmp/db-config.sql
 sed -i s/'{$WP_ADMIN_PASSWORD}'/$WP_ADMIN_PASSWORD/g /tmp/db-config.sql
-sed -i s/'{$WP_ADMIN_PASSWORD}'/$WP_ADMIN_PASSWORD/g /tmp/debian.cnf
-
-cp /tmp/debian.cnf /etc/mysql/debian.cnf
+sed -i s/'{$WP_ADMIN_PASSWORD}'/$WP_ADMIN_PASSWORD/g /etc/mysql/debian.cnf
 
 service mysql start
 mariadb -p$WP_ADMIN_PASSWORD < /tmp/db-config.sql && sleep 1
